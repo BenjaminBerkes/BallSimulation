f909d40c3bb32b4d1cf6a32271e8cacc4bd7d023
BenjaminBerkes <benjaminberkes0@gmail.com>
2025-02-26 17:43:50 -0500

    you can only create balls if the distance from center of viewport is less than radius of the circle
    Signed-off-by: BenjaminBerkes <benjaminberkes0@gmail.com>

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 allBalls = [];
-
+textColor = "purple";
+boundaryColor = "purple";
 let font = "Monocraft";
-
+boundarydiameter = 400;
 
 function setup() {
     vh=windowHeight;
@@ -14,33 +15,38 @@ function setup() {
 }
 function draw() {
     background('black');
-    fill('purple');
-    circle(vw/2,vh/2,400);
-    //ball1 = new Ball(x=mouseX,y=mouseY,radius=25);
-    //allBalls.push(ball1); 
+    fill(boundaryColor);
+    circle(vw/2,vh/2,boundarydiameter);
     // creates circle in with half the values
     // of viewport width and height so that
     // it will always be centered
+    
     //for (let i=0; i < allBalls.length; i++) {
     //    j = new Ball(mouseX,mouseY,35);
     //    j.display();
     //}
+    
+    
+    //ball1 = new Ball(x=mouseX,y=mouseY,radius=25);
+    //allBalls.push(ball1); 
+
 
     for (let i = 0; i < allBalls.length; i++) {
         //allBalls[i].newRadius();
         allBalls[i].display();
     }
-    //if (allBalls.length>0) {
-    //allBalls[0].display();
-    //}
+
     textFont(font);
-    //textSize(32);
+    textSize(20);
     let str1 = allBalls.length;
-    text("ball/object counter: "+str1.toString(), 100, 100)
-    
+    fill(textColor)
+    text("ball/object counter: "+str1.toString(), 60, 40)
+    text("this is distance of mouse from center: "+Math.sqrt((((vw/2)-mouseX)**2)+(((vh/2)-mouseY)**2)), 60, 60)
 }
 
-class Boundary {}
+class Boundary {
+    
+}
 
 class Ball {
     constructor(x,y, radius) {
@@ -52,6 +58,7 @@ class Ball {
     }
 
     display() {
+        
         fill('white')
         noStroke();
         ellipse(this.x,this.y,this.radius,this.radius);
@@ -60,16 +67,24 @@ class Ball {
         
     
     }
-    newRadius(i) {
-        this.radius=this.radius-1
-    }
 
     //move() {}
 }
 
+    
+    //Math.sqrt((((vw/2)-mouseX)**2)+(((vh/2)-mouseY)**2))
+
 function mousePressed() {
-    let newball = new Ball(mouseX, mouseY, 40);
-    allBalls.push(newball);
+    if ((boundarydiameter/2)>Math.sqrt((((vw/2)-mouseX)**2)+(((vh/2)-mouseY)**2))) {
+        // if the distance is less than the 
+        // diameter/2 AKA radius then the
+        // new instance of ball is allowed
+        // to be created
+        let newball = new Ball(mouseX, mouseY, 40);
+        allBalls.push(newball);
+    }
+    
+    
 
     
 }
