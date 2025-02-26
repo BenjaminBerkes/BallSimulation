6f2e1736ccf7919608ecc500e72e121f1b1a841a
BenjaminBerkes <benjaminberkes0@gmail.com>
2025-02-26 01:02:27 -0500

    there is now a purple circle in the CENTER of viewport/screen, there is now a white ball where the MOUSE is

--- sketch.js
+++ sketch.js
@@ -1,17 +1,42 @@
 function setup() {
-    let setting1 = createCanvas(600,600)
-    background('black')
-    fill('blue')
-    circle(200,200,400)
+    vh=windowHeight;
+    vw=windowWidth;
+    let canvas = createCanvas(vh,vw);
+    canvas.parent('canvas-container');
+    
+    
 }
 function draw() {
-    setting1
+    background('black');
+    fill('purple');
+    circle(vw/2,vh/2,400); 
+    // creates circle in with half the values
+    // of viewport width and height so that
+    // it will always be centered
+    ball1 = new Ball(mouseX,mouseY)
+    ball1.display()
 
 }
+
+class Boundary {}
+
 class Ball {
     constructor(x,y) {
+    this.x=x;
+    this.y=y;
     this.pos=createVector(x,y);
-    this.vel=p5.vector.random2D().mult(2);
-    this.radius=10;
+    //this.vel=p5.vector.random2D().mult(2);
+    this.radius=100;
     }
+
+    display() {
+        fill('white')
+        noStroke();
+        ellipse(this.x,this.y,100,100)
+    }
+    move() {}
+}
+
+function mousePressed() {
+    let ball = new Ball(mouseX, mouseY)
 }
